2548fa26f480895b640c1f6049e01b41275a694a
elushkina <yopolina@gmail.com>
2021-07-02 20:22:05 +0300

    new commit

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -2,20 +2,25 @@ import React from 'react'
 import styles from './ProfileInfo.module.css'
 
 
-
-
 const ProfileInfo = () => {
-    return (
-        <div>
-            <div>
-                <img className={styles.profile__img} src='https://sun9-64.userapi.com/impg/47RhNufk0KoDEYNn61hIZbUFp9ABLWFreGr7xw/GTEC0ZfUCpc.jpg?size=750x914&quality=96&sign=62e4ebe08c9f04e7b2415b19acd29ae7&type=album'
-                     alt=''/>
+    return (<div className={styles.user_info}>
+            <div className={styles.avatar}>
+                <div>
+                    <img className={styles.profile__img}
+                         src='https://sun9-64.userapi.com/impg/47RhNufk0KoDEYNn61hIZbUFp9ABLWFreGr7xw/GTEC0ZfUCpc.jpg?size=750x914&quality=96&sign=62e4ebe08c9f04e7b2415b19acd29ae7&type=album'
+                         alt=''/>
+                </div>
+                <button>Редактировать</button>
             </div>
-            <div className={styles.description__block}>
-                avatar
+            <div className={styles.user_about}>
+                <span>Белочка Басюшковна</span>
+                <div className={styles.user_information}>
+                    <div>Дата рождения: 29.01.2019</div>
+                    <div>Город: Дмитров</div>
+                    <div>Мать: Басюша Полюшковна</div>
+                </div>
             </div>
         </div>
-
     )
 }
 
